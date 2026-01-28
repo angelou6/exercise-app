672003cc65ce06cc0148a6ee4411f0ba54c4427e
Angelo <68724210+eljuanpepe@users.noreply.github.com>
2026-01-28 06:44:47 -0600

    reduce apk size

---
+++ app.config.js
@@ -0,0 +1,71 @@
+const isApkBuild = process.env.APK_BUILD === "true";
+
+export default {
+  expo: {
+    name: "Exercise App",
+    slug: "exercise-app",
+    version: "0.1.3",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "exerciseapp",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.angelou123.exerciseapp",
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage: "./assets/images/android-icon-foreground.png",
+        backgroundImage: "./assets/images/android-icon-background.png",
+        monochromeImage: "./assets/images/android-icon-monochrome.png",
+      },
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      permissions: [
+        "android.permission.RECORD_AUDIO",
+        "android.permission.MODIFY_AUDIO_SETTINGS",
+      ],
+      package: "com.angelou123.exerciseapp",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 300,
+          resizeMode: "contain",
+          backgroundColor: "#000000",
+          dark: {
+            backgroundColor: "#000000",
+          },
+        },
+      ],
+      "expo-sqlite",
+      "expo-notifications",
+      "expo-audio",
+      [
+        "expo-build-properties",
+        {
+          android: {
+            enableShrinkResourcesInReleaseBuilds: isApkBuild,
+            enableMinifyInReleaseBuilds: isApkBuild,
+            buildArchs: ["armeabi-v7a", "arm64-v8a"],
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId: "2e7aaa2c-4f48-42df-ac23-789f583d81a8",
+      },
+    },
+  },
+};
